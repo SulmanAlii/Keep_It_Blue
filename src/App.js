9698c3adaf94b515a12a538882857a3805b0a715
atoms91 <edustucom@gmail.com>
2020-12-01 16:34:20 +0100

    asdf

--- src/App.js
+++ src/App.js
@@ -4,26 +4,12 @@ import React from 'react';
 import Contenido from './Contenido';
 
 
-Leaflet.Marker.prototype.options.icon = DefaultIcon;
 
-
-=======
->>>>>>> 9bf1ad922c4450ed8b7cba9ba2430a290adf551d
 function App() {
  
 
   return (
-<<<<<<< HEAD
-    <div className="App" >
-    
-    <Mapa />
-    
-    {/*<h1>{beachName}</h1> */ }
-
-    </div>
-=======
     <Contenido/>
->>>>>>> 9bf1ad922c4450ed8b7cba9ba2430a290adf551d
   );
 }
 
