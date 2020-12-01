da5eeafb7532d5b3f6a1c67c5a82921de80fb0d0
SulmanAlii <sulmanm673@gmail.com>
2020-12-01 15:12:45 +0100

    show beach name and logo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "../node_modules/leaflet/dist/leaflet.css";
+import dataa from "./beach.json";
+import Leaflet from "leaflet";
+import {Button} from'reactstrap';
+import icon from "leaflet/dist/images/marker-icon.png";
+import img from './tree.png'
+
+let DefaultIcon = Leaflet.icon({
+  iconUrl: img,
+  iconSize : [40,40]
+});
+
+Leaflet.Marker.prototype.options.icon = DefaultIcon;
 
 function App() {
+
+  const [beachName,setbeachName] = useState(null);
+
+  // console.log(beachName);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <MapContainer
+      center={[41.445609, 2.24273]}
+      zoom={13}
+      scrollWheelZoom={false}
+    >
+      <TileLayer
+        url={`https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png`}
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        id="mapbox/light-v9"
+      />
+      {dataa.map((playa) => (
+        <Marker position={[playa["-l"], playa["-o"]]} >
+          <Popup>
+          {playa["-t"]}
+          <Button style={{marginLeft:"0.2rem", border:"none",padding:"0.2rem", background:"orange"}} onClick={() => setbeachName(playa["-t"])}>add</Button>
+          </Popup>
+        </Marker>
+      ))}
+    </MapContainer>
+    <h1>{beachName}</h1>
     </div>
   );
 }
