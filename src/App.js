935a19f1e115600e2865a6284235052e3ec1582a
atoms91 <edustucom@gmail.com>
2020-12-03 18:07:49 +0100

    mapa

--- src/App.js
+++ src/App.js
@@ -2,11 +2,8 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import React from 'react';
 import Contenido from './Contenido';
-<<<<<<< HEAD
-=======
 
 
->>>>>>> 3d8cd38bef41f171399f4b948480df17cf5a14e6
 
 
 
@@ -14,17 +11,11 @@ function App() {
  
 
   return (
-<<<<<<< HEAD
-    <Contenido/>
-=======
     <div className="App" >
     
     <Contenido />
-    
-    {/*<h1>{beachName}</h1> */ }
 
     </div>
->>>>>>> 3d8cd38bef41f171399f4b948480df17cf5a14e6
   );
 }
 
