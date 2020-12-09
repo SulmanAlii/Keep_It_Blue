6f5c4b6765a815d8444ab6dfe228f23b1a96c7a2
SulmanAlii <sulmanm673@gmail.com>
2020-12-09 15:31:57 +0100

    starting redux

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import App from './App';
 //<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous"></link>
 
 ReactDOM.render(
+  
   <React.StrictMode>
     <App />
   </React.StrictMode>,
