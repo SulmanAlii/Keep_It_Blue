f4bcd1217b63292eddf2279a74f26a9177694613
Tomas Bernaus <tomasbernaussole@gmail.com>
2020-12-11 18:28:19 +0100

    relleno

---
+++ src/components/Navbar/Totem.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+
+const Totem = () => {
+
+return (
+
+    <h1>
+        Contacta con nos en 
+
+        www.hellogudnight.com
+    </h1>
+
+);
+
+
+
+}
+
+export default Totem; 
