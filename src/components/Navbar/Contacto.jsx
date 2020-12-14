8eff2869ab62f1905c8855fdbaa93a4a4d883639
Tomas Bernaus <tomasbernaussole@gmail.com>
2020-12-14 19:27:52 +0100

    contacto 1.1

--- src/components/Navbar/Contacto.jsx
+++ src/components/Navbar/Contacto.jsx
@@ -1,59 +1,23 @@
-import React, { useState, useEffect } from "react";
-import { v4 as uuid } from "uuid";
-import { Container, Row, Col, Button, ButtonGroup } from "reactstrap";
+import React, { useState } from "react";
+import { Container, Row, Col, Button } from "reactstrap";
 import styled from "styled-components";
 const EspaiV = styled.div`
   height: ${({ alt }) => alt || "50px"};
 `;
 
 const Items = () => {
-  const [items, setItems] = useState([]);
-  const [url, setUrl] = useState();
   const [coment, setComent] = useState();
   const [qui, setQui] = useState();
   const [on, setOn] = useState();
 
-  console.log("hola");
-
-  useEffect(() => {
-    recuperar();
-  }, []);
-
-  const recuperar = () => {
-    const itemsJson = localStorage.getItem("mis_items");
-    const cosas = JSON.parse(itemsJson);
-    if (cosas && cosas.length) {
-      setItems(cosas);
-    } else {
-      setItems([]);
+  const Buida = () => {
+    if (coment) {
+      setComent("");
+      setQui("");
+      setOn("");
+      alert("Gracias por tu mensaje");
     }
-  };
-
-  useEffect(() => {
-    if (items.length) {
-      guardar();
-    }
-  }, [items]);
-
-  const guardar = () => {
-    localStorage.setItem("mis_items", JSON.stringify(items));
-  };
-
-  const afegir = () => {
-    if (url) {
-      const nouItem = {
-        imagen: url,
-        coment: coment,
-        qui: qui,
-        on: on,
-        id: uuid(),
-        likes: 0,
-      };
-      setItems([...items, nouItem]);
-      setUrl("");
-    }
-  };
-
+  }
 
   return (
     <Container>
@@ -62,16 +26,7 @@ const Items = () => {
         <Col>
           <div className="input-group">
             <div className="input-group-prepend">
-              {/* <div className="input-group-text">URL</div> */}
             </div>
-            {/* <input
-              type="text"
-              className="form-control"
-              id="inlineFormInputGroup"
-              placeholder="URL de la imatge"
-              value={url}
-              onChange={(ev) => setUrl(ev.target.value)}
-            /> */}
           </div>
           <EspaiV alt="10px" />
           <div className="input-group">
@@ -114,13 +69,11 @@ const Items = () => {
             />
           </div>
           <EspaiV alt="10px" />
-          <Button color="danger" onClick={afegir}>
+          <Button color="danger" onClick={Buida}>
             Enviar
           </Button>
         </Col>
-
       </Row>
-
       <EspaiV alt="40px" />
       <EspaiV alt="100px" />
     </Container>
