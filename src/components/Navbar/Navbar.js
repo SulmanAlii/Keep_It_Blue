185da5585aba013f01207c73f22e91b4c6ecb636
Tomas Bernaus <tomasbernaussole@gmail.com>
2020-12-15 17:21:44 +0100

    Minor fixes

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,14 +2,11 @@ import React, { Component } from 'react';
 import { MenuItems } from "./MenuItems";
 import { Button } from "./Button";
 import Mapa from '../Mapa';
-/* import './Navbar.css'; */
 import KeepItBlue from './KeepItBlue.png';
 import Contacto from './Contacto';
 import CrearEvento from '../CrearEvento';
-//import Login from '../Login';
 import Proyecto from './Proyecto';
 import Nosotros from './Nosotros';
-// import Totem from './Totem';
 import Alex from './Alex.jsx';
 import Edu from './Edu.jsx';
 import Joana from './Joana.jsx';
@@ -18,26 +15,19 @@ import Tomas from './Tomas.jsx';
 import NotFound from "./P404";
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-
 class Navbar extends Component {
-
     state = { clicked: false }
-
     handleClick = () => {
         this.setState({ clicked: !this.state.clicked })
     }
-
 
     render() {
         return (
             <BrowserRouter>
                 <nav className="NavbarItems">
-                    <img className="logoForma" src={logo} alt='Logo' width='160px' />
-
                     <h1 className="navbar-logo">
                         <img className="logoForma" src={KeepItBlue} alt='' width='110px'></img>
                         <i className=""></i> </h1>
-
                     <div className="menu-icon" onClick={this.handleClick}>
                         <i className={this.state.clicked ? 'fa fa-times' : 'fa fa-bars'}></i>
                     </div>
@@ -52,7 +42,7 @@ class Navbar extends Component {
                             )
                         })}
                     </ul>
-                    <Button>Registrate</Button>
+                    <Button>Regístrate</Button>
                 </nav>
                 <br />
                 <Switch>
@@ -65,9 +55,8 @@ class Navbar extends Component {
                     <Route path="/JoanaCorbella" render={() => <Joana />} />
                     <Route path="/SulmanAli" render={() => <Sulman />} />
                     <Route path="/TomasBernaus" render={() => <Tomas />} />
+                    <Route path="/crear-evento" render={() => <CrearEvento />} />
                     <Route component={NotFound} />
-                    <Route path="/crear-evento" render={() => <CrearEvento />} /> 
-                    <Route path="/Totem" render={() => <Totem />} /> 
                 </Switch>
             </BrowserRouter>
         )
